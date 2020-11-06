d0a94c484bab17b05fce8b979ffdd29059182bbf
Douglass Turner <douglass.turner@gmail.com>
2020-11-06 10:37:17 -0500

    Discard Juicebox related files (#14)

--- js/index.js
+++ js/index.js
@@ -2,16 +2,12 @@ import GenericMapDatasource from "./genericDataSource.js";
 import EncodeTrackDatasource from "./encodeTrackDatasource.js";
 import ModalTable from "./modalTable.js";
 import { encodeTrackDatasourceConfigurator } from "./encodeTrackDatasourceConfig.js";
-import { encodeContactMapDatasourceConfigurator } from "./encodeContactMapDatasourceConfig.js";
-import { aidenLabContactMapDatasourceConfigurator } from "./aidenLabContactMapDatasourceConfig.js";
 import { encodeTrackDatasourceOtherConfigurator } from "./encodeTrackDatasourceOtherConfig.js";
 import { encodeTrackDatasourceSignalConfigurator } from "./encodeTrackDatasourceSignalConfig.js";
 
 export {
     encodeTrackDatasourceOtherConfigurator,
     encodeTrackDatasourceSignalConfigurator,
-    aidenLabContactMapDatasourceConfigurator,
-    encodeContactMapDatasourceConfigurator,
     encodeTrackDatasourceConfigurator,
     GenericMapDatasource,
     EncodeTrackDatasource,
