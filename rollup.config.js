d851d878ef1bee8a72e9d5b7f75ac2650d3a42b9
jrobinso <jrobinso@broadinstitute.org>
2021-05-31 22:54:24 -0700

    Add rollup, fix package.json

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        input: 'js/index.js',
+        output:
+            [
+                {
+                    file: 'dist/data-modal.js',
+                    format: 'es'
+                },
+            ]
+    }
+]
