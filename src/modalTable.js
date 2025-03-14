cba81958df2d6f39c0dbf20ce6c21053c6e0934e
turner <douglass.turner@gmail.com>
2025-03-14 17:02:10 -0400

    Datatables. Public API is pure JS. But it still requires jquery internally

--- src/modalTable.js
+++ src/modalTable.js
@@ -1,10 +1,8 @@
 class ModalTable {
 
     constructor(args) {
-
         this.datasource = args.datasource
         this.okHandler = args.okHandler
-
         this.pageLength = args.pageLength || 10
 
         if (args.selectionStyle) {
@@ -27,12 +25,11 @@ class ModalTable {
         const modalFooter = modelElement.querySelector('.modal-footer')
         const okButton = modalFooter.querySelector('button:nth-child(2)')
         okButton.addEventListener('click', () => {
-            const selected = this.getSelectedTableRowsData.call(this, this.$dataTable.$('tr.selected'))
+            const selected = this.getSelectedTableRowsData.call(this)
             if (selected && this.okHandler) {
                 this.okHandler(selected)
             }
         })
-
     }
 
     setTitle(title) {
@@ -41,26 +38,31 @@ class ModalTable {
     }
 
     setDescription(description) {
-        // this.modalElement.querySelector('.modal-body').lastElementChild.innerHTML = `${description}`
         this.modalElement.querySelector('.modal-body').firstElementChild.innerHTML = `${description}`
     }
 
     remove() {
+        if (this.api) {
+            this.api.destroy()
+        }
         this.modalElement.parentNode.removeChild(this.modalElement)
     }
 
     setDatasource(datasource) {
         this.datasource = datasource
         this.datatableContainer.innerHTML = ''
-        this.$table = undefined
+        if (this.api) {
+            this.api.destroy()
+        }
+        this.table = undefined
     }
 
     async buildTable() {
-
-        if (!this.$table && this.datasource) {
-
-            this.$table = $('<table class="display"></table>')
-            this.datatableContainer.appendChild(this.$table.get(0))
+        if (!this.table && this.datasource) {
+            // Create table element
+            this.table = document.createElement('table')
+            this.table.className = 'display'
+            this.datatableContainer.appendChild(this.table)
 
             try {
                 this.startSpinner()
@@ -68,70 +70,59 @@ class ModalTable {
                 const tableData = await this.datasource.tableData()
                 const tableColumns = await this.datasource.tableColumns()
                 const columnDefs = this.datasource.columnDefs;
-                const config =
-                    {
-                        data: tableData,
-                        columns: tableColumns.map(c => {
-                            if (columnDefs && columnDefs[c]) {
-                                return Object.assign({}, columnDefs[c], {data: c});
-                            } else {
-                                return {title: c, data: c}
-                            }
-                        }),
-                        pageLength: this.pageLength,
-                        select: this.select,
-                        autoWidth: false,
-                        paging: true,
-                        scrollX: true,
-                        scrollY: '400px',
-                    };
+                const config = {
+                    data: tableData,
+                    columns: tableColumns.map(c => {
+                        if (columnDefs && columnDefs[c]) {
+                            return Object.assign({}, columnDefs[c], {data: c});
+                        } else {
+                            return {title: c, data: c}
+                        }
+                    }),
+                    pageLength: this.pageLength,
+                    select: this.select,
+                    autoWidth: false,
+                    paging: true,
+                    scrollX: true,
+                    scrollY: '400px',
+                };
 
-
-                // API object
-                this.api = this.$table.DataTable(config);
-
-                // Preserve sort order. For some reason it gets garbled by default
-                // this.api.column( 0 ).data().sort().draw();
+                // Initialize DataTable with jQuery
+                this.api = $(this.table).DataTable(config);
 
                 // Adjust column widths
                 this.api.columns.adjust().draw()
 
-                // jQuery object
-                this.$dataTable = this.$table.dataTable()
-
                 this.tableData = tableData
 
-
             } catch (e) {
-
+                console.error('Error building table:', e)
             } finally {
                 this.stopSpinner()
             }
         }
     }
 
-    getSelectedTableRowsData($rows) {
+    getSelectedTableRowsData() {
         const tableData = this.tableData
         const result = []
-        if ($rows.length > 0) {
-            $rows.removeClass('selected')
-            const api = this.$table.api()
-            $rows.each(function () {
-                const index = api.row(this).index()
-                result.push(tableData[index])
-            })
-            if (typeof this.datasource.rowHandler === 'function') {
+        const selectedRows = this.api.rows({selected: true}).data()
 
+        if (selectedRows.length > 0) {
+            selectedRows.each(row => {
+                result.push(row)
+            })
+
+            if (typeof this.datasource.rowHandler === 'function') {
                 const config = result.map(row => {
                     const thang = this.datasource.rowHandler(row)
                     const filteredKeys = Object.keys(row).filter(key => this.datasource.columns.includes(key))
                     thang.metadata = {}
                     for (let key of filteredKeys) {
-                        thang.metadata[ key ] = row[ key ]
+                        thang.metadata[key] = row[key]
                     }
                     return thang
                 })
-
                 return config
             } else {
                 return result;
@@ -148,11 +139,9 @@ class ModalTable {
     stopSpinner() {
         if (this.spinner) this.spinner.style.display = 'none'
     }
-
 }
 
 function createModalElement(title, id) {
-
     const html =
     `<div id="${id}" class="modal fade" tabindex="-1">
     
@@ -192,9 +181,7 @@ function createModalElement(title, id) {
     </div>`
 
     const fragment = document.createRange().createContextualFragment(html)
-
     return fragment.firstChild
-
 }
 
 export default ModalTable
